005c51d19a2e6bf447d35038c53131f82d52867e
Vlad <vladpasha12@gmail.com>
2023-03-05 16:41:23 +0200

    19-20

---
+++ Lessons/ls20/ls20.js
@@ -0,0 +1,18 @@
+"use strict";
+
+if (4 == 9) {
+    console.log('Ok');
+}   else {
+    console.log('Not ok')
+}
+
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много')
+} else {
+    console.log('Ok')
+}
---
+++ Lessons/ls21/ls21.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт');
+//}
+
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && fries && cola);
+
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "dsdfs");
+
+// if (hamburger === 3 && fries && cola === 1) {
+//     console.log('Все сыты');
+// } else {
+//     console.log('Уходим')
+// }
+        
+        
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger ===3 && cola === 2 || fries ===3 && nuggets) {
+    console.log('Все довольны');
+} else {
+    console.log('Уходим')
+}
+
+
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport|| mariaReport)
